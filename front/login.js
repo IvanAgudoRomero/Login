75e26a2a8c7eb5e426ab8fedbfbd3e271cfe87a0
Iván Agudo <76124415+IvanAgudoRomero@users.noreply.github.com>
2022-09-11 23:56:19 +0200

    Add files via upload

---
+++ front/login.js
@@ -0,0 +1,66 @@
+var iuser = document.getElementById("usr").value
+var ipassword = document.getElementById("pass").value
+
+var ierror = document.getElementById("errormsg")
+
+function login(){
+    iuser = document.getElementById("usr").value
+    ipassword = document.getElementById("pass").value
+    fetch("http://localhost:8079/login", {
+        headers:{
+            "Accept": "application/json",
+            "Content-Type": "application/json"            
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            user: iuser,
+            password: ipassword
+        })        
+    })
+    .then(response => response.text())
+    .then(result => notify(result)) 
+}
+
+
+
+function register(){
+    iuser = document.getElementById("usr").value
+    ipassword = document.getElementById("pass").value  
+    fetch("http://localhost:8079/register",{
+        headers:{
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            user: iuser,
+            password: ipassword
+        })
+    })
+    .then(response => response.text())
+    .then(result => notify(result)) 
+    
+}
+
+function notify(msg){
+    document.getElementById("errormsg").innerHTML = msg
+}
+
+function todos(){
+    fetch("http://localhost:8079/todos",{
+        headers:{
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "GET",
+        body: JSON.stringify({})        
+    })
+    .then(response => response.text())
+    .then(result => console.log(result))     
+}
+
+function clean(){
+    iuser.value = "",
+    ipassword.value = ""
+    notify("")
+}
